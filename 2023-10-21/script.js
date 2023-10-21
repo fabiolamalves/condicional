543dd7ca85c9b5b30e8ed9bbf2b73775ef42ba43
Fabiola Alves <fabiola.ma.santos@gmail.com>
2023-10-21 16:56:05 -0300

    Add files via upload

---
+++ 2023-10-21/script.js
@@ -0,0 +1,50 @@
+// alert("funfou")
+
+//let resposta = confirm("Você tem mais de 18?")
+
+//if(resposta) {
+//    alert("Pode dirigir!")
+//}else {
+//    alert("Cancela!")
+//}
+
+// TODO: Perguntar a idade, e caso a idade seja maior que
+// 17 Pode dirigir caso contrario nao pode
+
+//variaveis, tipos de dados e condicionais (para avaliar os conhecimentos adquiridos)
+
+//let number = prompt("Qual a sua idade?")
+
+//if (number >= 18) {
+ //   alert("Pode dirigir!")
+//}else {
+//    alert("Não pode dirigir!")
+//}
+
+// Deu bom!!!
+
+//truthy e falsy
+//true <0> "   "
+//false 0, "", NaN, null, undefined
+
+let idade = prompt("Qual a sua idade?")
+
+//idade = +idade
+//idade = Number(idade)
+//idade = parseInt(idade)
+
+if (idade <18) {
+    alert("Você é um adolescente!")
+}
+
+else if (idade ===18) {
+    alert("Você tem 18")
+}
+
+else {
+    alert("Você é adulto!")
+}
+
+//logica
+//__________
+// <18>
